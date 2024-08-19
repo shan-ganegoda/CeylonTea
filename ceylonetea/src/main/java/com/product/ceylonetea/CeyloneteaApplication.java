7eab736078c13621229fd4ee27b456db2e47b19e
Lakshan Ruwinda <112312002+shan-ganegoda@users.noreply.github.com>
2024-08-19 18:26:13 +0530

    initial Commit

---
+++ ceylonetea/src/main/java/com/product/ceylonetea/CeyloneteaApplication.java
@@ -0,0 +1,13 @@
+package com.product.ceylonetea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeyloneteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeyloneteaApplication.class, args);
+	}
+
+}
